1ac76c8c74fd0f21d1f68fefbba6942032bf445a
Pedro Paulo Lima Soares <pedso1812@gmail.com>
2026-09-23 23:41:48 -0300

    display e fiação feita

--- esp32Drawer/esp32Drawer.ino
+++ esp32Drawer/esp32Drawer.ino
@@ -5,38 +5,79 @@
 #include <MultiStepper.h>
 #include <string>
 #include <ESP32Servo.h>
+#include <TFT_eSPI.h>
 
 // 4 = 4-wire full-step (faster, ~2048 steps/rev)
 // 8 = 4-wire half-step (smoother, ~4096 steps/rev)
 #define MOTOR_INTERFACE_TYPE 4
 
 // Motor X Pins: IN1, IN2, IN3, IN4
-#define X_IN1 4
-#define X_IN2 15
-#define X_IN3 2
-#define X_IN4 14
+#define X_IN1 10
+#define X_IN2 11
+#define X_IN3 12
+#define X_IN4 13
 
 // Motor Y Pins: IN1, IN2, IN3, IN4
-#define Y_IN1 12
-#define Y_IN2 13
-#define Y_IN3 16
-#define Y_IN4 0
+#define Y_IN1 43
+#define Y_IN2 44
+#define Y_IN3 21
+#define Y_IN4 16
 
-#define BOT_FC_X 8
-#define BOT_FC_Y 9
+#define BOT_FC_X 1
+#define BOT_FC_Y 2
 
-#define SERVO 7
+#define SERVO 3
+
+#define DISPLAY_POWER_ON 15
 
 Servo servZ;
 
 const int MAX_X_OFFSET = 100000; // alterar para algo compatível com a estrutura final
 const int MAX_Y_OFFSET = 100000; // alterar para algo compatível com a estrutura final
 
+const int MAX_SPEED = 400;
+
+const int FONT_SCALE = 10;
+
 // IMPORTANT: Wire order passed to AccelStepper MUST be: IN1, IN3, IN2, IN4
 AccelStepper stepperX(MOTOR_INTERFACE_TYPE, X_IN1, X_IN3, X_IN2, X_IN4);
 AccelStepper stepperY(MOTOR_INTERFACE_TYPE, Y_IN1, Y_IN3, Y_IN2, Y_IN4);
 
 MultiStepper steppers;
+
+TFT_eSPI tft = TFT_eSPI();
+
+
+class Bot_FC_X{
+  public:
+    void setup(){
+      pinMode(BOT_FC_X, INPUT_PULLUP);
+    }
+    bool isPressed(){
+      if(digitalRead(BOT_FC_X) == LOW){
+        return true;
+      }else{
+        return false;
+      }
+    }
+};
+
+class Bot_FC_Y{
+  public:
+    void setup(){
+      pinMode(BOT_FC_Y, INPUT_PULLUP);
+    }
+    bool isPressed(){
+      if(digitalRead(BOT_FC_Y) == LOW){
+        return true;
+      }else{
+        return false;
+      }
+    }
+};
+
+Bot_FC_X botFCX;
+Bot_FC_Y botFCY;
 
 class Ctrl{
   public:
@@ -47,6 +88,12 @@ class Ctrl{
     void zeroPosition(){
       // enquanto botão fim de curso x não grita: vai pra origem x
       // enquanto botão fim de curso y não grita: vai pra origem y
+      while(botFCX.isPressed() == false){
+        stepperX.move(-1);
+      }
+      while(botFCY.isPressed() == false){
+        stepperY.move(-1);
+      }
       stepperX.setCurrentPosition(0);
       stepperY.setCurrentPosition(0);
     }
@@ -72,8 +119,14 @@ class Ctrl{
       steppers.runSpeedToPosition();
     }
 
-    void checkLimits(){
-      // a partir dos valores de offset maximo, calcula se a palavra atual iria "vazar"
+    void checkLimits(int addX, int addY){
+      //false = out of bounds; true = in bounds
+      if(xOffset + addX > MAX_X_OFFSET){
+        return false;
+      }else if(yOffset + addY > MAX_Y_OFFSET){
+        return false;
+      }
+      return true;
     }
 
     void requestNewPage(){
@@ -105,10 +158,10 @@ class Text_Conversion{
       if (data != NULL) {
         for (int i = 0; i < size; i += 4) {
 
-          int x1 = data[i + 0] + control->xOffset;
-          int y1 = data[i + 1] + control->yOffset;
-          int x2 = data[i + 2] + control->xOffset;
-          int y2 = data[i + 3] + control->yOffset;
+          int x1 = data[i + 0] * FONT_SCALE + control->xOffset;
+          int y1 = data[i + 1] * FONT_SCALE + control->yOffset;
+          int x2 = data[i + 2] * FONT_SCALE + control->xOffset;
+          int y2 = data[i + 3] * FONT_SCALE + control->yOffset;
 
           if(lastX2 != x1 || lastY2 != y1){
             control->liftPen(servZ);
@@ -120,7 +173,7 @@ class Text_Conversion{
           lastX2 = x2;
           lastY2 = x2;
         }
-        control->xOffset += width;
+        control->xOffset += width * FONT_SCALE;
       }
     }
 
@@ -137,46 +190,36 @@ class Text_Conversion{
     }
 };
 
-class Bot_FC_X{
-  public:
-    void setup(){
-      pinMode(BOT_FC_X, INPUT);
-    }
-    bool isPressed(){
-      if(digitalRead(BOT_FC_X) == HIGH){
-        return true;
-      }else{
-        return false;
-      }
-    }
-};
-
-class Bot_FC_Y{
-  public:
-    void setup(){
-      pinMode(BOT_FC_Y, INPUT);
-    }
-    bool isPressed(){
-      if(digitalRead(BOT_FC_Y) == HIGH){
-        return true;
-      }else{
-        return false;
-      }
-    }
-};
-
 class Display{
   public:
+
+    char* lastDisplay;
+
     void setup(){
-      // vou ter que ver como o display da julinha funciona
+      // 1. Turn on power to the peripheral and display rail
+      pinMode(DISPLAY_POWER_ON, OUTPUT);
+      digitalWrite(DISPLAY_POWER_ON, HIGH);
+      delay(100);
+
+      // 2. Initialize display
+      tft.init();
+      tft.setRotation(1); // 1 or 3 for landscape (320x170), 0 or 2 for portrait (170x320)
+      tft.fillScreen(TFT_BLACK);
     }
-    void display(std::string text){
-      // vou ter que ver como o display da julinha funciona
+    void showText(char* text){
+      if(lastDisplay == text){
+        return;
+      }else{
+        lastDisplay = text;
+      }
+      tft.fillScreen(TFT_BLACK);
+      tft.drawRect(10, 10, 300, 150, TFT_BLUE);
+      tft.setTextColor(TFT_GREEN, TFT_BLACK);
+      tft.setTextSize(2);
+      tft.drawString(text, 20, 30);
     }
 };
 
-Bot_FC_X botFCX;
-Bot_FC_Y botFCY;
 Ctrl control;
 Text_Conversion textConversion;
 Display display;
@@ -185,8 +228,8 @@ void setup(){
   
   Serial.begin(115200);
 
-  stepperX.setMaxSpeed(600.0);
-  stepperY.setMaxSpeed(600.0);
+  stepperX.setMaxSpeed(MAX_SPEED);
+  stepperY.setMaxSpeed(MAX_SPEED);
 
   steppers.addStepper(stepperX);
   steppers.addStepper(stepperY);
@@ -199,11 +242,13 @@ void setup(){
   control.zeroPosition();
 
   display.setup();
+  display.showText("VAMO TELLER JULIA BIAAA");
   
 }
 
 void loop(){
   
   textConversion.listenToNewText();
+  // caso escute algo no serial, passar para as funções de desenho e ir acumulando em um array de coisas para escrever, como uma fila
   
 }
